b8f01e58df2e2a256c630659ee8d159b732a5251
RSagar235 <124336584+RSagar235@users.noreply.github.com>
2023-09-26 19:17:33 +0530

    commit changes

---
+++ src/test/java/com/example/PracticeCICDGit.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+public class PracticeCICDGit {
+
+	@Test
+	public void method1() {
+		System.out.println("CICD practice from other class");
+	}
+
+	@Test
+	public void method2() {
+		System.out.println("CICD practice from other class2");
+	}
+}
---
+++ src/test/java/com/example/PracticeGitCICD.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+public class PracticeGitCICD {
+
+		@Test
+		public void method1() {
+			System.out.println("CICD practice from method1");
+		}
+
+		@Test
+		public void method2() {
+			System.out.println("CICD practice from method2");
+		}
+
+}
